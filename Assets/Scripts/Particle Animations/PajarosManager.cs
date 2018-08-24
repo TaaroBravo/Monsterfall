8c67af1c4e3669384e135fb1853bc6dca8d1ae98
DarioColo <32600360+DarioColo@users.noreply.github.com>
2018-08-24 09:01:06 -0300

    Ultimas Mejoras Visuales antes de la clase del viernes 24/8
    Todo tuyo lauta

---
+++ Assets/Scripts/Particle Animations/PajarosManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PajarosManager : MonoBehaviour {
+
+    public List<ParticleSystem> Pajaros = new List<ParticleSystem>();
+    float timer;
+    public float timetoreactivate;
+    public int ammountofbirds;
+    void Start()
+    {
+        for (int i = 0; i < Pajaros.Count; i++) Pajaros[i].Play();
+        //timer = timetoreactivate;
+    }
+    void Update () {
+        timer += Time.deltaTime;
+        if (timer > timetoreactivate)
+        {
+            timer = 0;
+            List<ParticleSystem> temp = new List<ParticleSystem>();
+            for (int i = 0; i < ammountofbirds; i++)
+            {
+                ParticleSystem temp2 = Pajaros[Random.Range(0, Pajaros.Count)];
+                if (!temp.Contains(temp2)) temp.Add(temp2);
+            }
+            for (int i = 0; i < Pajaros.Count; i++)
+                if (temp.Contains(Pajaros[i]) && !Pajaros[i].isPlaying) Pajaros[i].Play();
+                //else Pajaros[i].Stop();
+        }
+	}
+}
