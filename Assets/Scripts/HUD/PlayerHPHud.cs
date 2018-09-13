da8884650944c9a291a6a9b1acb1cf9c08bcf48b
DarioColo <32600360+DarioColo@users.noreply.github.com>
2018-09-13 06:15:16 -0300

    Mejoras de Particulas y HUD

---
+++ Assets/Scripts/HUD/PlayerHPHud.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHPHud : MonoBehaviour {
+
+    bool dead;
+    Color playerOutline;
+    public int characterID;
+    public float totalHP;
+    public float currentHP;
+    public Text HpNumber;
+    public Image CharPortrait;
+    public List<Sprite> AlivePics = new List<Sprite>();
+    public List<Sprite> DeadPics = new List<Sprite>();
+    void Start()
+    {
+        playerOutline = HpNumber.GetComponent<Outline>().effectColor; 
+    }
+    void Update()
+    {
+        if (currentHP > totalHP) currentHP = totalHP;
+        else if (((totalHP / 100) * currentHP) < 30)
+        {
+            HpNumber.GetComponent<Outline>().effectColor = new Color(0, 0, 0, 1);
+            HpNumber.color = new Color(1, 0, 0, 1);
+        }
+        else
+        {
+            HpNumber.GetComponent<Outline>().effectColor = playerOutline;
+            HpNumber.color = new Color(1, 1, 1, 1);
+        }
+        dead = currentHP > 0 ? false : true;
+        if (!dead)
+        {
+            HpNumber.text = ((totalHP / 100) * currentHP).ToString("F0");
+            CharPortrait.sprite = AlivePics[characterID];
+        }
+        else
+        {
+            HpNumber.text = 0.ToString();
+            CharPortrait.sprite = DeadPics[characterID];
+        }
+    }
+    public void AssignChar(int ID)
+    {
+        characterID = ID;
+    }
+
+}
---
+++ Assets/TestDeEscena/HUDAppearenceParticle/CdHUDChecker.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CdHUDChecker : MonoBehaviour {
+
+    public List<Sprite> SkillHUDs = new List<Sprite>();
+    public GameObject CDText;
+    public ParticleSystem PS_ReadySkill;
+    public bool onCD;
+    bool showCDHud;
+    float hidetimer;
+    float currentCD;
+    public float maxCD;
+
+    void Start()
+    {
+        AssignSkill(0); // DEBUG
+        GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 0);
+        CDText.GetComponent<TextMesh>().color = new Color(1, 1, 1, 0);
+    }
+    void Update()
+    {
+        DebugKeys();
+        if (onCD) currentCD -= Time.deltaTime;
+        if (currentCD < 0)
+        {
+            if (onCD) PS_ReadySkill.Play();
+            currentCD = 0;
+            onCD = false;
+        }
+        CDText.GetComponent<TextMesh>().text = currentCD.ToString("F1");
+        if (showCDHud)
+        {
+            hidetimer += Time.deltaTime;
+            GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 1 - hidetimer / 0.5f);
+            CDText.GetComponent<TextMesh>().color = new Color(1, 1, 1, 1 - hidetimer / 0.5f);
+        }
+        if (hidetimer >= 0.5f)
+        {
+            showCDHud = false;
+            hidetimer = 0;
+        }
+    }
+    void AssignSkill(int ID)
+    {
+        GetComponent<SpriteRenderer>().sprite = SkillHUDs[ID];
+    }
+    void UseSkill()
+    {
+        if (!showCDHud) showCDHud = true;
+        if (!onCD)
+        {
+            onCD = true;
+            currentCD = maxCD;
+        }
+    }
+    void DebugKeys()
+    {
+        if (Input.GetKeyDown(KeyCode.M)) UseSkill();
+    }
+}
