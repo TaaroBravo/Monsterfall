cb5cd2c3449071115e8682f8f375353a29011130
DarioColo <32600360+DarioColo@users.noreply.github.com>
2018-10-05 08:19:20 -0300

    Prefabs-Particulas

--- Assets/Particle/Berserker_Charge/DashFeedbackControl.cs
+++ Assets/Particle/Berserker_Charge/DashFeedbackControl.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class DashFeedbackControl : MonoBehaviour {
 
     public float timer;
-    bool activate;
+    public bool activate;
     public Material sarasa;
     public ParticleSystem ps;
 	// Use this for initialization
@@ -15,19 +15,16 @@ public class DashFeedbackControl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.Q) && !activate)
-        {
-            activate = true;
-            timer = 0;
-            ps.Play();
-        }
         if (activate)
         {
             timer += Time.deltaTime;
-            //sarasa.SetFloat("_Speed", 0);
             if (timer > 0.7f) sarasa.SetFloat("_Speed", (timer -0.7f)*3f);
             else sarasa.SetFloat("_Speed", 0);
-            if (timer >1) activate = false;
+            if (timer > 1)
+            {
+                activate = false;
+                timer = 0;
+            }
         }
 	}
 }
