b2aa306302b01d200b58d86a0a5c327bb8c51dd8
DarioColo <32600360+DarioColo@users.noreply.github.com>
2018-10-05 08:54:17 -0300

    Prefabs

---
+++ Assets/Scripts/Hability/Knight/KnightFeedbackController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnightFeedbackController : MonoBehaviour {
+
+    public ParticleSystem p1;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void PlayLanding()
+    {
+        p1.Play();
+    }
+}
