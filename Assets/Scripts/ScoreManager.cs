099c60aa60a87cfed3138cda15c8b008249550e7
Taaro Bravo <lautabdls@gmail.com>
2018-10-19 00:38:58 -0300

    V2.5d - MVP

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -29,13 +29,13 @@ public class ScoreManager : MonoBehaviour
     {
         playerInfo = playerInfo.OrderByDescending(x => x.newKills + x.previousKills).ToList();
 
+        var maxKills = playerInfo.Select(x => x.newKills).First();
+        if (maxKills >= 2)
+            CalculateMVP(playerInfo.Select(x => x.player_number).First(), playerInfo.Select(x => x.characterChosen).First(), maxKills);
+        else
+            CalculateMVP(playerInfo.Select(x => x.player_number).First(), playerInfo.Select(x => x.characterChosen).First(), 0);
         for (int i = 0; i < playerInfo.Count; i++)
         {
-            var maxKills = playerInfo[i].newKills;
-            if (maxKills >= 2)
-                CalculateMVP(playerInfo[i].player_number, playerInfo[i].characterChosen, maxKills);
-            else
-                CalculateMVP(playerInfo[i].player_number, playerInfo[i].characterChosen, 0);
             pointBars[i].SetActive(true);
             var pb = pointBars[i].GetComponent<PointsBar>();
             pb.SetCharacterSpriteAndColor(playerInfo[i]);
@@ -50,9 +50,10 @@ public class ScoreManager : MonoBehaviour
         callBack();
     }
 
+    //TODO: Hacer que se calcule el daño que hizo al contrario y aparezca primero.
     void CalculateMVP(int player_number, int characterChosen, int motive)
     {
-        MVPManager.Instance.ShowStand(player_number, characterChosen, motive);
+        MVPManager.Instance.ShowStand(player_number, characterChosen, motive - 1);
     }
 
     public void SetRound(int _round)
