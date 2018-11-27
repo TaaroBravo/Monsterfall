d1f4e6e76d952e4f6ef6e6739938bce64dacb17c
DarioColo <32600360+DarioColo@users.noreply.github.com>
2018-11-27 00:40:50 -0300

    Cambios Graficos
    Rotacion de las particulas de la elfa, berserker, fix de la particula de explosion del misil de la elfa, falta ajustes en casi todas las particulas al rotar

--- Assets/Agregados 16_11/GeneralFeedback.cs
+++ Assets/Agregados 16_11/GeneralFeedback.cs
@@ -8,15 +8,53 @@ public class GeneralFeedback : MonoBehaviour {
     public ParticleSystem.MainModule MainElfMark;
     public ParticleSystem ElfMark;
     public ParticleSystem CristalBuff;
+    public ParticleSystem CristalBuff2;
+    public ParticleSystem.ShapeModule CristalBuffShape;
+    public ParticleSystem.ShapeModule CristalBuffShape2;
+    float XAxis;
+    public bool iminverted;
+    public bool reposition;
 
     private void Start()
     {
+        CristalBuffShape = CristalBuff.shape;
+        CristalBuffShape2 = CristalBuff2.shape;
         MainElfMark = ElfMark.main;
         Color outcolor;
         if (ColorUtility.TryParseHtmlString("#1996E1FF", out outcolor)) PColors.Add(outcolor); // blue - 0
         if (ColorUtility.TryParseHtmlString("#E51B1BFF", out outcolor)) PColors.Add(outcolor); // red - 1
         if (ColorUtility.TryParseHtmlString("#5CD025FF", out outcolor)) PColors.Add(outcolor); // green - 2
         if (ColorUtility.TryParseHtmlString("#FBEB11FF", out outcolor)) PColors.Add(outcolor); // yellow - 3
+    }
+    private void Update()
+    {
+        XAxis = GetComponent<PlayerInput>().MainHorizontal();
+        if (!iminverted)
+        {
+            if (XAxis > 0)
+            {
+                CristalBuffShape.rotation = new Vector3(0, 180, 0);
+                CristalBuffShape2.rotation = new Vector3(0, 180, 0);
+            }
+            else if (XAxis < 0)
+            {
+                CristalBuffShape.rotation = new Vector3(0, 0, 0);
+                CristalBuffShape2.rotation = new Vector3(0, 0, 0);
+            }
+        }
+        else
+        {
+            if (XAxis > 0)
+            {
+                CristalBuffShape.rotation = new Vector3(0, 0, 0);
+                CristalBuffShape2.rotation = new Vector3(0, 0, 0);
+            }
+            else if (XAxis < 0)
+            {
+                CristalBuffShape.rotation = new Vector3(0, 180, 0);
+                CristalBuffShape2.rotation = new Vector3(0, 180, 0);
+            }
+        }
     }
     public void StartElfMark(int ID)
     {
--- Assets/Scripts/Hability/Elf/FeedbackElf.cs
+++ Assets/Scripts/Hability/Elf/FeedbackElf.cs
@@ -8,45 +8,152 @@ public class FeedbackElf : MonoBehaviour
     public Transform spawnPointDash;
     public Transform spawnPointHability;
     public ParticleSystem shootFire;
-    public ParticleSystem.ShapeModule DashShootShape;
+    public ParticleSystem.ShapeModule ShootShape;
+    public ParticleSystem shootFire2;
+    public ParticleSystem.ShapeModule ShootShape2;
+    public ParticleSystem shootFire3;
+    public ParticleSystem.ShapeModule ShootShape3;
     public ParticleSystem shootDashFire;
+    public ParticleSystem.ShapeModule DashShootShape;
+    public ParticleSystem shootDashFire2;
+    public ParticleSystem.ShapeModule DashShootShape2;
     public ParticleSystem DashTrail;
     public ParticleSystem.EmissionModule DashTrailEmission;
     public ParticleSystem DashFinish;
     public float XAxis;
+    public float YAxis;
     public bool iminverted;
     public bool teleporting;
 
     public void Start()
     {
         DashShootShape = shootDashFire.shape;
+        DashShootShape2 = shootDashFire2.shape;
+        ShootShape = shootFire.shape;
+        ShootShape2 = shootFire2.shape;
+        ShootShape3 = shootFire3.shape;
         DashTrailEmission = DashTrail.emission;
     }
     private void Update()
     {
         //DebugKeys();
         XAxis = GetComponent<PlayerInput>().MainHorizontal();
+        YAxis = GetComponent<PlayerInput>().MainVertical();
         DashTrailEmission.rateOverDistance = teleporting ? 10 : 0; // trail del teleport
         if (!iminverted) // esto quizas lo tenga k cambiar
         {
             if (XAxis > 0)
             {
-                DashShootShape.rotation = new Vector3(180, 0, 0);
+                ShootShape.rotation = new Vector3(0, 180, 0);
+                ShootShape2.rotation = new Vector3(0, 180, 0);
+                ShootShape3.rotation = new Vector3(0, 180, 0);
             }
             else if (XAxis < 0)
             {
-                DashShootShape.rotation = new Vector3(0, 0, 0);
+                ShootShape.rotation = new Vector3(0, 0, 0);
+                ShootShape2.rotation = new Vector3(0, 0, 0);
+                ShootShape3.rotation = new Vector3(0, 0, 0);
             }
         }
         else
         {
             if (XAxis > 0)
             {
-                DashShootShape.rotation = new Vector3(0, 0, 0);
+                ShootShape.rotation = new Vector3(0, 0, 0);
+                ShootShape2.rotation = new Vector3(0, 0, 0);
+                ShootShape3.rotation = new Vector3(0, 0, 0);
             }
             else if (XAxis < 0)
             {
-                DashShootShape.rotation = new Vector3(180, 0, 0);
+                ShootShape.rotation = new Vector3(0, 180, 0);
+                ShootShape2.rotation = new Vector3(0, 180, 0);
+                ShootShape3.rotation = new Vector3(0, 180, 0);
+            }
+        }
+        if (!iminverted)
+        {
+            if (YAxis > 0.25f && XAxis < -0.25f)
+            {
+                DashShootShape.rotation = new Vector3(0, 315, 0);
+                DashShootShape2.rotation = new Vector3(0, 315 + 90, 0);
+            }
+            else if (YAxis > 0.25f && XAxis > 0.25f)
+            {
+                DashShootShape.rotation = new Vector3(0, 225, 0);
+                DashShootShape2.rotation = new Vector3(0, 225 - 90, 0);
+            }
+            else if (YAxis < -0.25f && XAxis < -0.25f)
+            {
+                DashShootShape.rotation = new Vector3(0, 45, 0);
+                DashShootShape2.rotation = new Vector3(0, 45 - 90, 0);
+            }
+            else if (YAxis < -0.25f && XAxis > 0.25f)
+            {
+                DashShootShape.rotation = new Vector3(0, 135, 0);
+                DashShootShape2.rotation = new Vector3(0, 135 + 90, 0);
+            }
+            else if (YAxis > 0.25f && XAxis < 0.25f || YAxis > 0.25f && XAxis > -0.25f)
+            {
+                DashShootShape.rotation = new Vector3(0, 270, 0);
+                DashShootShape2.rotation = new Vector3(0, 90, 0);
+            }
+            else if (YAxis < -0.25f && XAxis < 0.25f || YAxis < -0.25f && XAxis > -0.25f)
+            {
+                DashShootShape.rotation = new Vector3(0, 90, 0);
+                DashShootShape2.rotation = new Vector3(0, 270, 0);
+            }
+            else if (YAxis > -0.25f && XAxis > 0.25f || YAxis < 0.25f && XAxis > 0.25f)
+            {
+                DashShootShape.rotation = new Vector3(0, 180, 0);
+                DashShootShape2.rotation = new Vector3(0, 180, 0);
+            }
+            else if (YAxis > -0.25f && XAxis < -0.25f || YAxis < 0.25f && XAxis < -0.25f)
+            {
+                DashShootShape.rotation = new Vector3(0, 0, 0);
+                DashShootShape2.rotation = new Vector3(0, 0, 0);
+            }
+        }
+        else
+        {
+            if (YAxis > 0.25f && XAxis < -0.25f)
+            {
+                DashShootShape.rotation = new Vector3(0, 225, 0);
+                DashShootShape2.rotation = new Vector3(0, 225 - 90, 0);
+            }
+            else if (YAxis > 0.25f && XAxis > 0.25f)
+            {
+                DashShootShape.rotation = new Vector3(0, 315, 0);
+                DashShootShape2.rotation = new Vector3(0, 315 + 90, 0);
+            }
+            else if (YAxis < -0.25f && XAxis < -0.25f)
+            {
+                DashShootShape.rotation = new Vector3(0, 125, 0);
+                DashShootShape2.rotation = new Vector3(0, 125 + 90, 0);
+            }
+            else if (YAxis < -0.25f && XAxis > 0.25f)
+            {
+                DashShootShape.rotation = new Vector3(0, 45, 0);
+                DashShootShape2.rotation = new Vector3(0, 45 - 90, 0);
+            }
+            else if (YAxis > 0.25f && XAxis < 0.25f || YAxis > 0.25f && XAxis > -0.25f) // funca
+            {
+                DashShootShape.rotation = new Vector3(0, 270, 0);
+                DashShootShape2.rotation = new Vector3(0, 90, 0);
+            }
+            else if (YAxis < -0.25f && XAxis < 0.25f || YAxis < -0.25f && XAxis > -0.25f) // funca
+            {
+                DashShootShape.rotation = new Vector3(0, 90, 0);
+                DashShootShape2.rotation = new Vector3(0, 270, 0);
+            }
+            else if (YAxis > -0.25f && XAxis > 0.25f || YAxis < 0.25f && XAxis > 0.25f)
+            {
+                DashShootShape.rotation = new Vector3(0, 0, 0);
+                DashShootShape2.rotation = new Vector3(0, 0, 0);
+            }
+            else if (YAxis > -0.25f && XAxis < -0.25f || YAxis < 0.25f && XAxis < -0.25f)
+            {
+                DashShootShape.rotation = new Vector3(0, 180, 0);
+                DashShootShape2.rotation = new Vector3(0, 180, 0);
             }
         }
     }
@@ -64,6 +171,4 @@ public class FeedbackElf : MonoBehaviour
         DashFinish.Play();
         teleporting = false;
     }
-
-
 }
