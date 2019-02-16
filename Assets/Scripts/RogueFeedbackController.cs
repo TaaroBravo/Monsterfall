dd7c468c4cc9730a424552a556542c8a2b045e63
DarioColo <32600360+DarioColo@users.noreply.github.com>
2019-02-16 01:11:04 -0300

    Rogue Dash, Selector Back Button

--- Assets/Scripts/RogueFeedbackController.cs
+++ Assets/Scripts/RogueFeedbackController.cs
@@ -8,6 +8,7 @@ public class RogueFeedbackController : MonoBehaviour {
     public ParticleSystem p1;
     public ParticleSystem.MainModule p1main;
     public ParticleSystem.ShapeModule p1shape;
+    public ParticleSystem.EmissionModule p1emission;
     public float caseN;
     public Material textshader1;
     public Material textshader2;
@@ -16,117 +17,120 @@ public class RogueFeedbackController : MonoBehaviour {
     {
         p1main = p1.main;
         p1shape = p1.shape;
+        p1emission = p1.emission;
     }
     public void Update()
     {
-        YAxis = GetComponent<PlayerInput>().MainVertical();
-        XAxis = GetComponent<PlayerInput>().MainHorizontal();
-        if (!iminverted)
-        {
-            if (YAxis > 0.25f && XAxis < -0.25f)
-            {
-                p1main.startRotationZ = 2.4f;
-                p1shape.rotation = new Vector3(0, 45, 0);
-                p1main.startSpeed = 10;
-            }
-            else if (YAxis > 0.25f && XAxis > 0.25f)
-            {
-                p1main.startRotationZ = 0.8f;
-                p1shape.rotation = new Vector3(0, 125, 0);
-                p1main.startSpeed = 10;
-            }
-            else if (YAxis < -0.25f && XAxis < -0.25f)
-            {
-                p1main.startRotationZ = 0.8f;
-                p1shape.rotation = new Vector3(0, 315, 0);
-                p1main.startSpeed = 10;
-            }
-            else if (YAxis < -0.25f && XAxis > 0.25f)
-            {
-                p1main.startRotationZ = 2.4f;
-                p1shape.rotation = new Vector3(0, 225, 0);
-                p1main.startSpeed = 10;
-            }
-            else if (YAxis > 0.25f && XAxis < 0.25f || YAxis > 0.25f && XAxis > -0.25f)
-            {
-                p1main.startRotationZ = 1.6f;
-                p1shape.rotation = new Vector3(0, 90, 0);
-                p1main.startSpeed = 8;
-            }
-            else if (YAxis < -0.25f && XAxis < 0.25f || YAxis < -0.25f && XAxis > -0.25f)
-            {
-                p1main.startRotationZ = 1.6f;
-                p1shape.rotation = new Vector3(0, 270, 0);
-                p1main.startSpeed = 8;
-            }
-            else if (YAxis > -0.25f && XAxis > 0.25f || YAxis < 0.25f && XAxis > 0.25f)
-            {
-                p1main.startRotationZ = 0;
-                p1shape.rotation = new Vector3(0, 180, 0);
-                p1main.startSpeed = 8;
-            }
-            else if (YAxis > -0.25f && XAxis < -0.25f || YAxis < 0.25f && XAxis < -0.25f)
-            {
-                p1main.startRotationZ = 0;
-                p1shape.rotation = new Vector3(0, 0, 0);
-                p1main.startSpeed = 8;
-            }
-        }
-        else
-        {
-            if (YAxis > 0.25f && XAxis < -0.25f)
-            {
-                p1main.startRotationZ = 0.8f;
-                p1shape.rotation = new Vector3(0, 125, 0);
-                p1main.startSpeed = 10;
-            }
-            else if (YAxis > 0.25f && XAxis > 0.25f)
-            {
-                p1main.startRotationZ = 2.4f;
-                p1shape.rotation = new Vector3(0, 45, 0);
-                p1main.startSpeed = 10;
-            }
-            else if (YAxis < -0.25f && XAxis < -0.25f)
-            {
-                p1main.startRotationZ = 2.4f;
-                p1shape.rotation = new Vector3(0, 225, 0);
-                p1main.startSpeed = 10;
-            }
-            else if (YAxis < -0.25f && XAxis > 0.25f)
-            {
-                p1main.startRotationZ = 0.8f;
-                p1shape.rotation = new Vector3(0, 315, 0);
-                p1main.startSpeed = 10;
-            }
-            else if (YAxis > 0.25f && XAxis < 0.25f || YAxis > 0.25f && XAxis > -0.25f) // funca
-            {
-                p1main.startRotationZ = 1.6f;
-                p1shape.rotation = new Vector3(0, 90, 0);
-                p1main.startSpeed = 8;
-            }
-            else if (YAxis < -0.25f && XAxis < 0.25f || YAxis < -0.25f && XAxis > -0.25f) // funca
-            {
-                p1main.startRotationZ = 1.6f;
-                p1shape.rotation = new Vector3(0, 270, 0);
-                p1main.startSpeed = 8;
-            }
-            else if (YAxis > -0.25f && XAxis > 0.25f || YAxis < 0.25f && XAxis > 0.25f)
-            {
-                p1main.startRotationZ = 0;
-                p1shape.rotation = new Vector3(0, 0, 0);
-                p1main.startSpeed = 8;
-            }
-            else if (YAxis > -0.25f && XAxis < -0.25f || YAxis < 0.25f && XAxis < -0.25f)
-            {
-                p1main.startRotationZ = 0;
-                p1shape.rotation = new Vector3(0, 180, 0);
-                p1main.startSpeed = 8;
-            }
-        }
+        #region Old Rogue Dash Particle
+        //YAxis = GetComponent<PlayerInput>().MainVertical();
+        //XAxis = GetComponent<PlayerInput>().MainHorizontal();
+        //if (!iminverted)
+        //{
+        //    if (YAxis > 0.25f && XAxis < -0.25f)
+        //    {
+        //        p1main.startRotationZ = 2.4f;
+        //        p1shape.rotation = new Vector3(0, 45, 0);
+        //        p1main.startSpeed = 10;
+        //    }
+        //    else if (YAxis > 0.25f && XAxis > 0.25f)
+        //    {
+        //        p1main.startRotationZ = 0.8f;
+        //        p1shape.rotation = new Vector3(0, 125, 0);
+        //        p1main.startSpeed = 10;
+        //    }
+        //    else if (YAxis < -0.25f && XAxis < -0.25f)
+        //    {
+        //        p1main.startRotationZ = 0.8f;
+        //        p1shape.rotation = new Vector3(0, 315, 0);
+        //        p1main.startSpeed = 10;
+        //    }
+        //    else if (YAxis < -0.25f && XAxis > 0.25f)
+        //    {
+        //        p1main.startRotationZ = 2.4f;
+        //        p1shape.rotation = new Vector3(0, 225, 0);
+        //        p1main.startSpeed = 10;
+        //    }
+        //    else if (YAxis > 0.25f && XAxis < 0.25f || YAxis > 0.25f && XAxis > -0.25f)
+        //    {
+        //        p1main.startRotationZ = 1.6f;
+        //        p1shape.rotation = new Vector3(0, 90, 0);
+        //        p1main.startSpeed = 8;
+        //    }
+        //    else if (YAxis < -0.25f && XAxis < 0.25f || YAxis < -0.25f && XAxis > -0.25f)
+        //    {
+        //        p1main.startRotationZ = 1.6f;
+        //        p1shape.rotation = new Vector3(0, 270, 0);
+        //        p1main.startSpeed = 8;
+        //    }
+        //    else if (YAxis > -0.25f && XAxis > 0.25f || YAxis < 0.25f && XAxis > 0.25f)
+        //    {
+        //        p1main.startRotationZ = 0;
+        //        p1shape.rotation = new Vector3(0, 180, 0);
+        //        p1main.startSpeed = 8;
+        //    }
+        //    else if (YAxis > -0.25f && XAxis < -0.25f || YAxis < 0.25f && XAxis < -0.25f)
+        //    {
+        //        p1main.startRotationZ = 0;
+        //        p1shape.rotation = new Vector3(0, 0, 0);
+        //        p1main.startSpeed = 8;
+        //    }
+        //}
+        //else
+        //{
+        //    if (YAxis > 0.25f && XAxis < -0.25f)
+        //    {
+        //        p1main.startRotationZ = 0.8f;
+        //        p1shape.rotation = new Vector3(0, 125, 0);
+        //        p1main.startSpeed = 10;
+        //    }
+        //    else if (YAxis > 0.25f && XAxis > 0.25f)
+        //    {
+        //        p1main.startRotationZ = 2.4f;
+        //        p1shape.rotation = new Vector3(0, 45, 0);
+        //        p1main.startSpeed = 10;
+        //    }
+        //    else if (YAxis < -0.25f && XAxis < -0.25f)
+        //    {
+        //        p1main.startRotationZ = 2.4f;
+        //        p1shape.rotation = new Vector3(0, 225, 0);
+        //        p1main.startSpeed = 10;
+        //    }
+        //    else if (YAxis < -0.25f && XAxis > 0.25f)
+        //    {
+        //        p1main.startRotationZ = 0.8f;
+        //        p1shape.rotation = new Vector3(0, 315, 0);
+        //        p1main.startSpeed = 10;
+        //    }
+        //    else if (YAxis > 0.25f && XAxis < 0.25f || YAxis > 0.25f && XAxis > -0.25f) // funca
+        //    {
+        //        p1main.startRotationZ = 1.6f;
+        //        p1shape.rotation = new Vector3(0, 90, 0);
+        //        p1main.startSpeed = 8;
+        //    }
+        //    else if (YAxis < -0.25f && XAxis < 0.25f || YAxis < -0.25f && XAxis > -0.25f) // funca
+        //    {
+        //        p1main.startRotationZ = 1.6f;
+        //        p1shape.rotation = new Vector3(0, 270, 0);
+        //        p1main.startSpeed = 8;
+        //    }
+        //    else if (YAxis > -0.25f && XAxis > 0.25f || YAxis < 0.25f && XAxis > 0.25f)
+        //    {
+        //        p1main.startRotationZ = 0;
+        //        p1shape.rotation = new Vector3(0, 0, 0);
+        //        p1main.startSpeed = 8;
+        //    }
+        //    else if (YAxis > -0.25f && XAxis < -0.25f || YAxis < 0.25f && XAxis < -0.25f)
+        //    {
+        //        p1main.startRotationZ = 0;
+        //        p1shape.rotation = new Vector3(0, 180, 0);
+        //        p1main.startSpeed = 8;
+        //    }
+        //}
+        #endregion 
     }
     public void DashRogue()
     {
-        p1.Play();
+        p1emission.rateOverDistance = 7;
     }
     public void ChangeTextColor()
     {
