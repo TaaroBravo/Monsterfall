a92df4e665d055cc482bda7dd4356f0cfaaba3a5
DarioColo <32600360+DarioColo@users.noreply.github.com>
2019-03-25 08:13:33 -0300

    Primera organizacion y actualizacion de version

---
+++ Assets/Galeria/Grafico de chequeo de cooldown viejo (arte 2D, obsoleto)/ParticleFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleFollow : MonoBehaviour {
+
+    public string pathname;
+    public float speed;
+	// Use this for initialization
+	void Start () {
+        //iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath(pathname), "easeType", iTween.EaseType.easeInOutSine, "time", speed));
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
